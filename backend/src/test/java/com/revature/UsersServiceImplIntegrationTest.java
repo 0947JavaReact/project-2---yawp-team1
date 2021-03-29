120d4f9b90c9fb48b455f2a84d9056d4ef7dafe6
ThomasJae <tjae.lew@gmail.com>
2021-03-29 15:05:44 -0700

    Some User Service Tests

--- backend/src/test/java/com/revature/UsersServiceImplIntegrationTest.java
+++ backend/src/test/java/com/revature/UsersServiceImplIntegrationTest.java
@@ -1,29 +1,26 @@
 package com.revature;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
-import org.junit.After;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.revature.controllers.UserController;
 import com.revature.impl.UserServiceImpl;
 import com.revature.models.StoredPassword;
 import com.revature.models.User;
+import com.revature.repositories.FollowerHolderDao;
+import com.revature.repositories.FollowingHolderDao;
+import com.revature.repositories.PasswordDao;
 import com.revature.repositories.UserDao;
 import com.revature.services.UserService;
 
@@ -45,6 +42,12 @@ public class UsersServiceImplIntegrationTest {
 	
 	@MockBean
 	private UserDao userDao;
+	@MockBean
+	private FollowerHolderDao followerHDao;
+	@MockBean
+	private FollowingHolderDao followingHDao;
+	@MockBean
+	private PasswordDao passDao;
 	
 	@Before
 	public void setUp() {
@@ -58,11 +61,35 @@ public class UsersServiceImplIntegrationTest {
 	@Test
 	public void whenValidName_thenUserShouldBeFound() {
 		String name = "robert123";
-		User found =  userServ.getUserByUsername(name);
+		User found = userServ.getUserByUsername(name);
 		
 		assertThat(found.getUsername()).isEqualTo(name);
 	}
 	
-	
+	@Test
+	public void whenValidUser_thenAllUsersShouldBeFound() {
+		List<User> uList = new ArrayList<>();
+		uList = userServ.getAllUsers();
+		
+		assertThat(uList.size()).isEqualTo(1);
+	}
 
+	@Test
+	public void whenPwordGenerated_thenPwordShouldBeSame() {
+		String tempPass = "";
+		tempPass = userServ.generatePassayPassword();
+		
+		assertThat(tempPass).isNotEqualTo("");
+	}
+	
+	@Test
+	public void whenPwordHashed_thenPwordHashIsSame() {
+		String tempPass = "password";
+		String hashPass = userServ.hashPassword(tempPass);
+		User user = userServ.getUserByUsername("robert123");
+		String userHashPass = userServ.hashPassword(user.getPasswordHolder().getHashedPassword());
+		
+		assertThat(hashPass).isEqualTo(userHashPass);
+	}
+	
 }
