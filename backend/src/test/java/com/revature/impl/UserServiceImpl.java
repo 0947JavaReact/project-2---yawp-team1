da47ce0feff88e806b7b0834e712fa7190ee6d62
ThomasJae <tjae.lew@gmail.com>
2021-03-31 13:45:36 -0700

    Commented Services and ServiceImpl files

--- backend/src/test/java/com/revature/impl/UserServiceImpl.java
+++ backend/src/test/java/com/revature/impl/UserServiceImpl.java
@@ -38,293 +38,320 @@ import com.revature.services.UserService;
 
 public class UserServiceImpl extends UserService {
 
+	// the test file will use this implementation of UserService
 	@Autowired
 	private UserDao userDao;
-	
 	@Autowired
 	private FollowerHolderDao followerHDao;
-	
 	@Autowired
 	private FollowingHolderDao followingHDao;
-	
 	@Autowired
 	private PasswordDao passDao;
 	
-	
 	@Override
+	// get a list of all users from the database
 	public List<User> getAllUsers() {
 		return userDao.findAll();
 	}
 	
-	
 	@Override
+	// create a generated password
 	public String generatePassayPassword() {
 		PasswordGenerator gen = new PasswordGenerator();
+		// establish lower case characters rule
 		CharacterData lowerCaseChars = EnglishCharacterData.LowerCase;
 		CharacterRule lowerCaseRule = new CharacterRule(lowerCaseChars);
 		lowerCaseRule.setNumberOfCharacters(2);
-
+		// establish upper case characters rule
 		CharacterData upperCaseChars = EnglishCharacterData.UpperCase;
 		CharacterRule upperCaseRule = new CharacterRule(upperCaseChars);
 		upperCaseRule.setNumberOfCharacters(2);
-
+		// establish digit characters rule
 		CharacterData digitChars = EnglishCharacterData.Digit;
 		CharacterRule digitRule = new CharacterRule(digitChars);
 		digitRule.setNumberOfCharacters(2);
-
+		// establish special characters rule
 		CharacterData specialChars = new CharacterData() {
 			public String getErrorCode() {
 				return "ERROR";
 			}
-
 			public String getCharacters() {
 				return "!@#$%^&*()_+";
 			}
 		};
 		CharacterRule splCharRule = new CharacterRule(specialChars);
 		splCharRule.setNumberOfCharacters(2);
-
+		// generate and return password
 		String password = gen.generatePassword(10, splCharRule, lowerCaseRule, upperCaseRule, digitRule);
 		return password;
 	}
 
 	@Override
+	// encrypt a plain password string
 	public String hashPassword(String password_plaintext) {
 		String salt = BCrypt.gensalt(12);
+		// convert from a normal password to hashed value
 		String hashed_password = BCrypt.hashpw(password_plaintext, salt);
-
+		// return encrypted password
 		return (hashed_password);
 	}
 
-	
+	// check if a hashed password value is equal to a plain password string
 	public boolean checkPassword(String password_plaintext, String stored_hash) {
 		boolean password_verified = false;
-
+		// throw an exception if the hashed password is null or was made with a different pattern
 		if (null == stored_hash || !stored_hash.startsWith("$2a$"))
 			throw new java.lang.IllegalArgumentException("Invalid hash provided for comparison");
-
+		// verify that the passwords match
 		password_verified = BCrypt.checkpw(password_plaintext, stored_hash);
-
+		// return true if the actual password is the same
 		return (password_verified);
 	}
 
 	@Override
+	// register a new user
 	public User register(User u) {
+		// throw an exception if attempting to create a user with a username that is already taken
 		if (userDao.findByUsername(u.getUsername()) != null) {
 			throw new UserAlreadyExistsException();
 		}
+		// throw an exception if attempting to create a user with an email that is already taken
 		if (userDao.findByEmail(u.getEmail()) != null) {
 			throw new EmailAlreadyExistsException();
 		}
-
+		// add password to database
 		passDao.save(u.getPasswordHolder());
+		// add user to database
 		userDao.save(u);
+		// user creation successful
 		return u;
 	}
 
 	@Override
-	public User getUserById(int id) {
-		
-		Optional<User> oUser = userDao.findById(id);
-
+	// get a user by their id
+	public User getUserById(int user_id) {
+		// find a user in the database with the provided id value
+		Optional<User> oUser = userDao.findById(user_id);
+		// throw an exception if no user was found
 		if (oUser.get() == null) {
 			throw new UsernameDoesNotExistException(); 
 		}
-		
+		// return the user that was found
 		return oUser.get();
 	}
 
 	@Override
+	// get a user by their username
 	public User getUserByUsername(String username) {
+		// find a user in the database with the provided username
 		User user = userDao.findByUsername(username);
-		System.out.println("User: " + username);
-		System.out.println(user);
+		// throw an exception if no user was found
 		if (user == null) {
 			throw new UsernameDoesNotExistException();
 		}
-		return userDao.findByUsername(username);
-	}
-
-	@Override
-	public List<User> searchByUsername(String str) {
-		List<User> uList = new ArrayList<>();
-		uList = userDao.findByUsernameContaining(str);
-		if (uList.isEmpty()) {
-			throw new SearchReturnedZeroResultsException();
-		}
-		return uList;
-	}
-	
-	@Override
-	public boolean updateUser(User user) {
-		User original = userDao.findByUsername(user.getUsername());
-		userDao.save(user);
-		if (original.equals(user)) {
-			throw new UpdateFailedException();
-		}
-		return true;
-	}
-
-	@Override
-	public boolean addFollower(int user, int follower) {
-		StoredFollowers sf = new StoredFollowers();
-		sf.setUserId(user);
-		sf.setFollowerId(follower);
-		System.out.println(sf);
-		followerHDao.save(sf);
-		return true;
-	}
-
-	@Override
-	public boolean addFollowing(int user, int following) {
-		StoredFollowing sf = new StoredFollowing();
-		sf.setUserId(user);
-		sf.setFollowingId(following);
-		System.out.println(sf);
-		followingHDao.save(sf);
-		return true;
-	}
-
-	@Override
-	public boolean removeFollower(int user, int follower) {
-		List<StoredFollowers> sfList = followerHDao.findAllByUserId(user);
-		System.out.println("in remove follower list: " + sfList);
-		for (StoredFollowers sf : sfList) {
-			if (sf.getFollowerId() == follower) {
-				System.out.println(sf);
-				followerHDao.delete(sf);
-			}
-		}
-		return true;
-	}
-
-	@Override
-	public boolean removeFollowing(int user, int following) {
-		System.out.println("user id: " + user + " /nfollowing: " + following);
-		List<StoredFollowing> sfList = followingHDao.findAllByUserId(user);
-		System.out.println("in remove following list: " + sfList);
-		for (StoredFollowing sf : sfList) {
-			if (sf.getFollowingId() == following) {
-				System.out.println(sf);
-				followingHDao.delete(sf);
-			}
-		}
-		return true;
-	}
-
-	@Override
-	public List<User> getUserFollowers(int id) {
-
-		List<StoredFollowers> sUsers = followerHDao.findAllByUserId(id);
-		System.out.println(id);
-		System.out.println(sUsers);
-		List<User> userList = new ArrayList<>();
-		for (StoredFollowers i : sUsers) {
-			userList.add(getUserById(i.getFollowerId()));
-		}
-
-		return userList;
-	}
-
-	@Override
-	public List<User> getUserFollowing(int id) {
-		List<StoredFollowing> sUsers = followingHDao.findAllByUserId(id);
-		System.out.println(sUsers);
-		List<User> userList = new ArrayList<>();
-		for (StoredFollowing i : sUsers) {
-			userList.add(getUserById(i.getFollowingId()));
-		}
-		return userList;
-	}
-
-	@Override
-	public User login(String username, String password) {
-        System.out.println(password);
-
-		User user = new User();
-		user = userDao.findByUsername(username);
-
-		if (user == null) {
-			throw new UsernameDoesNotExistException();
-		}
-
-		if (!checkPassword(password, user.getPasswordHolder().getHashedPassword())) {
-			throw new InvalidCredentialsException();
-		}
+		// return the user that was found
 		return user;
 	}
 
 	@Override
-	public void resetPassword(String email, String tempPassword, String newPassword) {
-		User user =  userDao.findByEmail(email);
-		System.out.println("Email: " + email);
-		System.out.println("temp: " + tempPassword);
-		System.out.println("new: " + newPassword);
-		if(!checkPassword(tempPassword, user.getPasswordHolder().getHashedPassword())) {
-			System.out.println("throwing e");
-			throw new InvalidCredentialsException();
+	// search for all users with a username that contain a specific string
+	public List<User> searchByUsername(String str) {
+		List<User> uList = new ArrayList<>();
+		// get a list of all users that include a string in their username
+		uList = userDao.findByUsernameContaining(str);
+		// throw an exception if no user was found similar to the string
+		if (uList.isEmpty()) {
+			throw new SearchReturnedZeroResultsException();
 		}
-		String hashed = hashPassword(newPassword);
-		StoredPassword sp = user.getPasswordHolder();
-		
-		sp.setHashedPassword(hashed);
-		passDao.save(sp);
+		// return a list of users that had a username like the search string
+		return uList;
+	}
+	
+	@Override
+	// update a user's attributes in the database
+	public boolean updateUser(User user) {
+		// find the user by username in the database
+		User original = userDao.findByUsername(user.getUsername());
+		// update their values
 		userDao.save(user);
-		
+		// throw an exception if the database still holds their old values
+		if (original.equals(user)) {
+			throw new UpdateFailedException();
+		}
+		// user was successfully updated
+		return true;
 	}
 
 	@Override
+	// add a relationship to the Follower Table
+	public boolean addFollower(int user, int follower) {
+		StoredFollowers sf = new StoredFollowers();
+		// establish relationship between a user and their follower
+		sf.setUserId(user);
+		sf.setFollowerId(follower);
+		// add relationship to Follower Table
+		followerHDao.save(sf);
+		// successfully added follower relationship
+		return true;
+	}
+
+	@Override
+	// add a relationship to the Following Table
+	public boolean addFollowing(int user, int following) {
+		StoredFollowing sf = new StoredFollowing();
+		// establish relationship between a user following another user
+		sf.setUserId(user);
+		sf.setFollowingId(following);
+		// add relationship to Following Table
+		followingHDao.save(sf);
+		// successfully added following relationship
+		return true;
+	}
+
+	@Override
+	// remove a relationship from the Follower Table
+	public boolean removeFollower(int user_id, int follower_id) {
+		// get a list of follower relationships for a user
+		List<StoredFollowers> sfList = followerHDao.findAllByUserId(user_id);
+		// loop through list to find matching follower id
+		for (StoredFollowers sf : sfList) {
+			if (sf.getFollowerId() == follower_id) {
+				// remove follower relationship from database
+				followerHDao.delete(sf);
+			}
+		}
+		// successfully removed follower relationship
+		return true;
+	}
+
+	@Override
+	// remove a relationship from the Following Table
+	public boolean removeFollowing(int user_id, int following_id) {
+		// get a list of following relationships for a user
+		List<StoredFollowing> sfList = followingHDao.findAllByUserId(user_id);
+		// loop through list to find matching following id
+		for (StoredFollowing sf : sfList) {
+			if (sf.getFollowingId() == following_id) {
+				// remove following relationship from database
+				followingHDao.delete(sf);
+			}
+		}
+		// successfully removed following relationship
+		return true;
+	}
+
+	@Override
+	// get a list of followers for a user
+	public List<User> getUserFollowers(int user_id) {
+		// get a list of follower relationships for a user
+		List<StoredFollowers> sUsers = followerHDao.findAllByUserId(user_id);
+		List<User> userList = new ArrayList<>();
+		// loop through list of relationships to add each follower to a user list
+		for (StoredFollowers i : sUsers) {
+			userList.add(getUserById(i.getFollowerId()));
+		}
+		// return list of followers
+		return userList;
+	}
+
+	@Override
+	// get a list of following for a user
+	public List<User> getUserFollowing(int user_id) {
+		// get a list of following relationships for a user
+		List<StoredFollowing> sUsers = followingHDao.findAllByUserId(user_id);
+		List<User> userList = new ArrayList<>();
+		// loop through list of relationships to add each following to a user list
+		for (StoredFollowing i : sUsers) {
+			userList.add(getUserById(i.getFollowingId()));
+		}
+		// return list of following
+		return userList;
+	}
+
+	@Override
+	// get a user to login
+	public User login(String username, String password) {
+		User user = new User();
+		// find a user in the database by username
+		user = userDao.findByUsername(username);
+		// throw an exception if no user was found
+		if (user == null) {
+			throw new UsernameDoesNotExistException();
+		}
+		// throw an exception if the password was incorrect
+		if (!checkPassword(password, user.getPasswordHolder().getHashedPassword())) {
+			throw new InvalidCredentialsException();
+		}
+		// successfully log in as a user
+		return user;
+	}
+
+	@Override
+	// allow a user to reset their password credential
+	public void resetPassword(String email, String tempPassword, String newPassword) {
+		// find a user in the database by email
+		User user =  userDao.findByEmail(email);
+		// throw an exception if the original password was incorrect
+		if(!checkPassword(tempPassword, user.getPasswordHolder().getHashedPassword())) {
+			throw new InvalidCredentialsException();
+		}
+		// encrypt new password
+		String hashed = hashPassword(newPassword);
+		// update current user's password credential
+		StoredPassword sp = user.getPasswordHolder();
+		sp.setHashedPassword(hashed);
+		// update Password Table in database
+		passDao.save(sp);
+		// update User Table in database
+		userDao.save(user);
+	}
+
+	@Override
+	// send a user an email to reset their password
 	public void sendResetEmail(String email) {
 		User user = new User();
+		// find a user in the database by email
 		user = userDao.findByEmail(email);
-
-		System.out.println("At top of send reset: " + email);
-
+		// throw an exception if no user was found
 		if (user == null) {
-			System.out.println("email doesn't exist");
 			throw new EmailDoesNotExistException();
 		}
-		// create new temporary password.
+		// create new temporary password
 		String temporaryPassword = generatePassayPassword();
-		System.out.println("temp password: " + temporaryPassword);
-
-		// Create the email
+		// create the email to send to user
 		Email emailToSend = new Email(email);
-		System.out.println(emailToSend);
 		try {
 			MimeMessage message = new MimeMessage(emailToSend.getSession());
-
+			// format the email content
 			message.setFrom(new InternetAddress(emailToSend.getSendFrom()));
 			message.setRecipient(RecipientType.TO, new InternetAddress(emailToSend.getSendTo()));
 			message.setSubject("YAWP -- Password Reset");
-			String messageBody = "<h6>You've requested a password reset.<h6> <br> Below is your username and new temporary password."
-					+ "Once you've logged in, you will be asked to set you're new password.<br>" + "<b>User</b>: "
+			String messageBody = "<h6>You have requested a password reset.<h6> <br> Below is your username and new temporary password."
+					+ "Once you've logged in, you will be asked to set your new password.<br>" + "<b>User</b>: "
 					+ user.getUsername() + "<br>" + "<b>Pass</b>: " + temporaryPassword + "<br><br><br><br>"
-					+ "If you have any issues or questions please conctact us:\n" + "<h4>YAWP Team<h4><br>"
+					+ "If you have any issues or questions, please contact us:\n" + "<h4>YAWP Team<h4><br>"
 					+ "<b>Phone</b>: 555-5555<br>" + "<b>Address</b>: 462 South 4th Street, Suite 1600<br>"
 					+ "				   Louisville, KY 40202<br>";
 			messageBody = "<font face=\"courier new\" size=\"10px\">" + messageBody + "</font></p>";
 			message.setContent(messageBody, "text/html");
-
+			// set the email delivery method
 			Transport transport = emailToSend.getSession().getTransport("smtp");
-			
-			System.out.println("Host: " + emailToSend.getHost());
-			System.out.println("From: " + emailToSend.getSendFrom());
-			System.out.println("Password: " + emailToSend.getPassword());
 			transport.connect(emailToSend.getHost(), emailToSend.getSendFrom(), emailToSend.getPassword());
 			transport.sendMessage(message, message.getAllRecipients());
 			transport.close();
 		} catch (MessagingException e) {
+			// throw an exception if an error occurred sending the mail
 			e.printStackTrace();
 			throw new EmailNotSentException();
 		}
-
-		// if this is successful we hash the temp password set it to the user and wait
-		// for them to login
+		// if this is successful we hash the temp password, set it to the user and wait for them to login
 		String hashedTemp = hashPassword(temporaryPassword);
 		StoredPassword sp = user.getPasswordHolder();
 		sp.setHashedPassword(hashedTemp);
+		// update Password Table
 		passDao.save(sp);
+		// update User Table
 		userDao.save(user);
 	}
+	
 }
